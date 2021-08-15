1b0f9915d337372cac24b2f878c64f305b0e3a92
Adam Lawson <adamjlawson22@gmail.com>
2021-08-15 08:54:10 +0100

    sdm710-common: Include CAF framework compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,6 +119,9 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96
 
