8f4e0cd56926a4aff7cc7b4992f691cb7c3e258c
Adam Lawson <dev@goshawk22.uk>
2022-02-25 15:27:46 +0000

    sdm710-common: Disable secure_element

--- sdm710.mk
+++ sdm710.mk
@@ -409,6 +409,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Disable Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.0-service-disabled
+
 # Sensors
 PRODUCT_PACKAGES += \
    libsensorndkbridge
