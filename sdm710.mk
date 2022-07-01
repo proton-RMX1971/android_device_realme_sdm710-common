bec8166b2fde7ae53afd161cad2d3a7525a6701b
Adam Lawson <dev@goshawk22.uk>
2022-07-01 09:59:14 +0100

    Revert "sdm710-common: Build extphonelib packages"
    This reverts commit f9836840b0cac5f6cafea4363f1ef435cd31e951.

--- sdm710.mk
+++ sdm710.mk
@@ -457,12 +457,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     qcrilhook
 
-PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml
-
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
